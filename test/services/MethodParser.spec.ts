758b68d45b06bb8bd6d5237f802c74b5125c33fe
scveloso <samtveloso@gmail.com>
2019-11-19 14:35:55 -0800

    WIP Waiting for unzip functionality

--- test/services/MethodParser.spec.ts
+++ test/services/MethodParser.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
-import MethodParser from "../src/services/MethodParser";
+import MethodParser from "../../src/services/MethodParser";
 
 describe("MethodParser tests", () => {
 
     let methodParser: MethodParser = new MethodParser();
 
+    // TODO: Have actual tests here, waiting on unzip functionality from James so we can
+    // have test projects as zip files
     it("should return a list of commits up to the latest commit", async () => {
         let projStr = '../../../src/ca/ubc/cs/cs317/dnslookup/';
 
