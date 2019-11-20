5722ab8901f5964cc4e9846d35a8e33774f0cfb6
kiyomih <j.kiyomi.henry@alumni.ubc.ca>
2019-11-20 10:15:33 -0800

    move github access token references to one place

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -19,7 +19,6 @@ export default class GithubService {
     constructor(restClient: IRestClient, cache: ICommitCache) {
         this.restClient = restClient;
         this.urlBuilder = new URLBuilder( AccessTokenManager.getGithubAccessToken());
-        this.urlBuilder = new URLBuilder( "");
         this.responseParser = new ResponseParser();
         this.cache = cache;
     }
