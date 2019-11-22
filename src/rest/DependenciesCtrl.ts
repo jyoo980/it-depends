4ce70505350a68c09c704ffa962123c39d1bcaed
uslava77 <uslava77@gmail.com>
2019-11-22 09:25:46 -0800

    support 8080 port for local testing

--- src/rest/DependenciesCtrl.ts
+++ src/rest/DependenciesCtrl.ts
@@ -50,7 +50,7 @@ export default class DependenciesCtrl {
         this.server.get('/crosscut/functions/sample/:num', this.getCrossCutFunctionSampleData);
         this.server.get('/dependency/functions/sample/:num', this.getDependencyFunctionSampleData);
 
-        this.server.listen(process.env.PORT, () => {
+        this.server.listen((process.env.PORT || 8080), () => {
             console.log("SERVER LISTENING AT: " + this.server.url);
         });
     }
