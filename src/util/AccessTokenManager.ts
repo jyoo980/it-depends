cecbf99c3e4ad43447a9d00619cf56b8405bc3c8
uslava77 <uslava77@gmail.com>
2019-11-22 09:55:14 -0800

    github token fix

--- src/util/AccessTokenManager.ts
+++ src/util/AccessTokenManager.ts
@@ -1,5 +1,5 @@
 export default class AccessTokenManager {
     public static getGithubAccessToken() {
-        return "PUT YOUR GITHUB ACCESS TOKEN HERE";
+        return (process.env.GITHUB_API_KEY);
     }
 }
