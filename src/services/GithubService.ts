f0214282b9316d76b2cb781f1ca5e59b784f5fda
James Yoo <jyoo980@gmail.com>
2019-12-27 15:16:12 -0800

    removing dead code in GithubService.ts

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -26,17 +26,17 @@ export default class GithubService {
     public async getAndSaveAllCommits(repoUrl: string): Promise<Array<CommitInfo>> {
         try {
             const historyExists: boolean = await this.cache.exists(repoUrl);
-            if (!historyExists) {
-                let totalNumCommits = await this.getNumCommits(repoUrl);
-                let retrievedCommits: Array<CommitInfo> = new Array<CommitInfo>();
-                while (retrievedCommits.length < totalNumCommits) {
-                    const commits = await this.getMoreCommits(retrievedCommits, repoUrl);
-                    retrievedCommits = retrievedCommits.concat(commits);
-                }
-                await this.cache.persistCommits(repoUrl, retrievedCommits);
-                return retrievedCommits;
+            if (historyExists) {
+                return await this.cache.getCommitData(repoUrl);
             }
-            return await this.cache.getCommitData(repoUrl);
+            const totalNumCommits = await this.getNumCommits(repoUrl);
+            let retrievedCommits: Array<CommitInfo> = new Array<CommitInfo>();
+            while (retrievedCommits.length < totalNumCommits) {
+                const commits = await this.getMoreCommits(retrievedCommits, repoUrl);
+                retrievedCommits = retrievedCommits.concat(commits);
+            }
+            await this.cache.persistCommits(repoUrl, retrievedCommits);
+            return retrievedCommits;
         } catch (err) {
             console.warn(`GithubService::Error while getting commits from: ${repoUrl}`);
             throw { message: err.message } as GithubServiceError
@@ -59,19 +59,6 @@ export default class GithubService {
         const rawCommits = await this.restClient.get(requestUrl);
         const commitSHAs = this.responseParser.getCommitSHAs(rawCommits.body);
         return await this.hydrateCommits(repoUrl, commitSHAs);
-    }
-
-    public async listCommitsUpTo(repoUrl: string, upTo: number): Promise<Array<CommitInfo>> {
-        try {
-            const historyExists: boolean = await this.cache.exists(repoUrl);
-            if (!historyExists) {
-                await this.getAndSaveAllCommits(repoUrl);
-            }
-            return await this.cache.readCommitsUpTo(repoUrl, upTo);
-        } catch (err) {
-            console.warn(`GithubService::Error while listing commits from: ${repoUrl}`);
-            throw { message: err.message } as GithubServiceError;
-        }
     }
 
     public async listCommitsBetween(repoUrl: string, startIndex: number, endIndex: number): Promise<Array<CommitInfo>> {
